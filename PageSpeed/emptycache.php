bbe6a800a8721b6baa297dd24b843e081afdd44a
tom sassen <tom.sassen@hotmail.com>
2017-08-31 20:24:48 +0100

    remove things that shouldn't be tracked

--- PageSpeed/emptycache.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__) . '/../../plugins/UsefulFunctions/bootstrap.console.php';
-
-$Directory = new RecursiveDirectoryIterator('cache/ps');
-foreach(new RecursiveIteratorIterator($Directory) as $File) {
-	$CachedFile = $File->GetPathName();
-	unlink($CachedFile);
-	Console::Message('Removed ^3%s', $CachedFile);
-}
